d025f4ca404f33cc84f5404c82f9fa3f04260301
Mehdi <mehdi.desoeuvre@gmail.com>
2022-04-15 10:58:49 +0200

    mariadb progress with db creation

--- srcs/requirements/wordpress/tools/startup.sh
+++ srcs/requirements/wordpress/tools/startup.sh
@@ -12,7 +12,7 @@ else
 	chmod 644 wp-config.php
 	wp core install --url=$DOMAIN_NAME --title="mdesoeuv inception" --admin_name=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --allow-root
 	cd wp-content
-	mkdir uploads
+	mkdir -p uploads
 	chgrp www-data uploads/
 	chmod 775 uploads/
 fi
