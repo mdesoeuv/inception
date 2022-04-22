ece8e37f111d85279547c4cd3a1844747728bcc1
Mehdi <mehdi.desoeuvre@gmail.com>
2022-04-22 09:43:08 +0200

    db test before initialization

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#if test -f /var/lib/mysql/$WORDPRESS_DB_NAME;
-#then
-#	echo "mariadb already configured"
-#else
 service mysql start
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password;"
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $MYSQL_DATABASE"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD'"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
-#fi
+if test -f /var/lib/mysql/$WORDPRESS_DB_NAME;
+then
+	echo "mariadb already configured"
+else
+	mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password;"
+	mysql -u root -e "ALTER USER root@localhost IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $MYSQL_DATABASE"
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD'"
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION"
+	mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
+fi
